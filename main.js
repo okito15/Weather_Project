5af2562e889391bde9201b4fd451f67e3be0f094
Okan Seid <70080899+okito15@users.noreply.github.com>
2021-06-14 15:37:12 +0300

    Final Commit

--- main.js
+++ main.js
@@ -5,16 +5,15 @@ const findMyGeolocation = () => {
     const success = (position) => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-
-        const geoApiUrl = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en'
+        const geoApiUrl = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude='.concat(latitude ,'&longitude=', longitude,'&localityLanguage=en');
         fetch(geoApiUrl)
         .then(result => result.json())
         .then(data => {
-            const city = data.city;
-            status.textContent = city;
-            document.getElementById('button1').addEventListener('click', retrieveToday(city));
-            //document.getElementById("button2").addEventListener('click', retrieveThreeDays(city));
-            //document.getElementById("button3").addEventListener('click', retrieveSevenDays(city));
+            //const city = data.city;
+            status.textContent = data.city;
+            document.getElementById('button1').addEventListener('click', retrieveToday(data.city));
+            //document.getElementById("button2").addEventListener('click', retrieveThreeDays(data.city));
+            //document.getElementById("button3").addEventListener('click', retrieveSevenDays(data.city));
         })     
     }
 
@@ -25,19 +24,20 @@ const findMyGeolocation = () => {
     navigator.geolocation.getCurrentPosition(success, error);
 }
 
-const retrieveToday = (city) => {
-    const apiKey = 'a26e3cbea03ba51c4e9b63887af42dfd'
+    const retrieveToday = (city) => 
+    {
+    const apiKey = '55f2f7d7ccdf3ac608e24a5fe2f7993e';
     const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='.concat(city,'&appid=', apiKey, '&units=metric');
     
     fetch(weatherUrl)
     .then(result => result.json())
     .then(data => {
-        document.getElementById('temperature').append(' ', data.main.temp, '°C');
+        //document.getElementById('temperature').append(' ', data.main.temp, '°C');
+        document.getElementById('temperature').innerHTML += ' ' + data.main.temp + '°C';
         document.getElementById('humidity').append(' ', data.main.humidity, '%');
         document.getElementById('rainChance').append(' ', data.clouds.all, '%');
         document.getElementById('windSpeed').append(' ', data.wind.speed, 'm/s');
-        document.getElementById('cloudness').append(' ', data.clouds.all, '%');
-        
+        document.getElementById('cloudness').append(' ', data.clouds.all, '%');  
     })
 }
 
@@ -51,5 +51,4 @@ const retrieveToday = (city) => {
 }
 */
 
-
-addEventListener('load', findMyGeolocation);
+addEventListener('load', findMyGeolocation());
